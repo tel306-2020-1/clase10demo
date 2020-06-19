e1550cd55c7a477f13ea85c791ddfcb4f47e16fc
Stuardo <stuardo.lucho@pucp.pe>
2020-06-19 15:55:27 -0500

    avance

--- app/src/main/java/com/example/clase10demo/MainActivityStorage.java
+++ app/src/main/java/com/example/clase10demo/MainActivityStorage.java
@@ -12,11 +12,15 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -40,14 +44,15 @@ public class MainActivityStorage extends AppCompatActivity {
 
     }
 
-    public boolean validarPermisos(int requestCode) {
-        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+    public boolean validarPermisos(int requestCode, String permission_code) {
+
+        int permission = ContextCompat.checkSelfPermission(this, permission_code);
 
         if (permission == PackageManager.PERMISSION_GRANTED) {
             return true;
         } else {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    new String[]{permission_code},
                     requestCode);
             return false;
         }
@@ -55,20 +60,22 @@ public class MainActivityStorage extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 1) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == 1) {
                 subirImagenStream(null);
             }
-        }
-        if (requestCode == 2) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == 2) {
                 subirArchivoComoFile(null);
+            }
+            if (requestCode == 3) {
+                descargarArchivo(null);
             }
         }
     }
 
     public void subirArchivoComoFile(View view) {
-        if (validarPermisos(2)) {
+        if (validarPermisos(2, Manifest.permission.READ_EXTERNAL_STORAGE)) {
             File fileDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
             String fileName = "Clase 02.1 - Introducción a las aplicaciónes móviles.pdf";
 
@@ -79,7 +86,13 @@ public class MainActivityStorage extends AppCompatActivity {
             StorageReference imagenesRef =
                     storageReference.child("imagenes/" + fileName);
 
-            imagenesRef.putFile(fileUri)
+            StorageMetadata metadata = new StorageMetadata.Builder()
+                    .setCustomMetadata("Autor", "Stuardo Lucho")
+                    .setCustomMetadata("Curso", "TEL306")
+                    .build();
+
+
+            imagenesRef.putFile(fileUri, metadata)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
@@ -98,7 +111,7 @@ public class MainActivityStorage extends AppCompatActivity {
                             long bytesTransferred = taskSnapshot.getBytesTransferred();
                             long totalByteCount = taskSnapshot.getTotalByteCount();
                             double progreso = (100.0 * bytesTransferred) / totalByteCount;
-                            Log.d("infoAppPro","porcentaje de subida: " + progreso);
+                            Log.d("infoAppPro", "porcentaje de subida: " + progreso);
                         }
                     });
 
@@ -108,7 +121,7 @@ public class MainActivityStorage extends AppCompatActivity {
 
     public void subirImagenStream(View view) {
 
-        if (validarPermisos(1)) {
+        if (validarPermisos(1, Manifest.permission.READ_EXTERNAL_STORAGE)) {
             File fileDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
             File archivo = new File(fileDir, "pucp.jpg");
 
@@ -136,5 +149,53 @@ public class MainActivityStorage extends AppCompatActivity {
             }
         }
 
+    }
+
+    public void descargarArchivo(View view) {
+        if (validarPermisos(3, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            String fileName = "Clase 02.1 - Introducción a las aplicaciónes móviles.pdf";
+
+            StorageReference pdfReference =
+                    storageReference.child("imagenes/" + fileName);
+
+            File fileDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+            File file = new File(fileDir,"clase.pdf");
+
+            pdfReference.getFile(file)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Log.d("infoApp", "descarga exitosa!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("infoApp", "error", e.getCause());
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            long bytesTransferred = taskSnapshot.getBytesTransferred();
+                            long totalByteCount = taskSnapshot.getTotalByteCount();
+                            double progreso = (100.0 * bytesTransferred) / totalByteCount;
+                            Log.d("infoAppPro", "porcentaje de bajada: " + Math.round(progreso));
+                        }
+                    });
+
+        }
+
+    }
+
+    public void descargarYmostrarImagen(View view){
+
+        StorageReference imgRef = storageReference.child("imagenes/pucpcito.jpg");
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Glide.with(this).load(imgRef).into(imageView);
     }
 }
